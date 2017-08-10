5e1ceb2a47a75c0d0352fce1691728953ddf8638
dell <1041871747@qq.com>
2017-08-10 20:49:37 +0800

    增加一个查询接口

--- src/main/java/com/doocker/controller/DeptController.java
+++ src/main/java/com/doocker/controller/DeptController.java
@@ -27,6 +27,14 @@ public class DeptController {
 		return list;
 	}
 	
-	
+	@RequestMapping("list1")
+	@ResponseBody
+	public List<Dept> getAllDept1(){
+		List<Dept> list = null;
+		Dept dept = new Dept();
+		dept.setDeptName("新增");
+		Integer intt= deptService.insertDept(dept);
+		return list;
+	}
 	
 }
