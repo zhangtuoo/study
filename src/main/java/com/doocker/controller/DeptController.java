4a46d785ae6520496f1c25edad3ef833c76df5f6
yangguo <1130061927@qq.com>
2017-08-10 19:30:12 +0800

    我又增加了一个查询的接口

--- src/main/java/com/doocker/controller/DeptController.java
+++ src/main/java/com/doocker/controller/DeptController.java
@@ -38,5 +38,14 @@ public class DeptController {
 		return list;
 	}
 	
+	@RequestMapping("list2")
+	@ResponseBody
+	public List<Dept> getAllDept2(){
+		List<Dept> list = null;
+		Dept dept = new Dept();
+		dept.setDeptName("新增");
+		Integer intt= deptService.insertDept(dept);
+		return list;
+	}
 	
 }
